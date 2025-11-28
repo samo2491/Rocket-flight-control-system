7bb77ff16b64c4e1949ddd1b1c447e80758e3d87
samo2491 <166990140+samo2491@users.noreply.github.com>
2025-11-28 12:13:47 +0000

    Integrate GPS and BMP388 for altitude and location
    Added GPS and BMP388 functionality for altitude measurement.

--- advance fligth control system.ino
+++ advance fligth control system.ino
@@ -1,6 +1,21 @@
 #include <MPU6050_tockn.h> //scl =A5 / sda =A4 /vcc =5v / grn =grn
 #include <Servo.h>
+//GPS
+#include<TinyGPSPlus.h>
+#include <SoftwareSerial.h>
+//BMP388 Altimeter
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
 
+// define
+
+#define SEALEVELPRESSURE_HPA (1000)
+
+//start connection between GPS
+SoftwareSerial serial_connection(2,3); 
+TinyGPSPlus gps;
 // buzzer pin 
 
 int servopin1 = 9;  // Servo pin
@@ -21,6 +36,7 @@ long timer = 0;
 
 void setup() {
   Serial.begin(9600);
+  serial_connection.begin(9600);
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
@@ -30,17 +46,41 @@ void setup() {
   servo3.attach(servopin3);
   servo4.attach(servopin4);
 
+  // Setup BMP388 values for Temp Altitude and Pressure
+  
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 
+  
 }
 
 void loop() {
+  
+  // Print GPS Data
+  
+  while(serial_connection.available()){
+    
+    gps.encode(serial_connection.read());}
+    if (gps.location.isUpdated()){
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(", ");
+    Serial.println(gps.location.lng(), 6);
+      
+  }
 
+  // Print Altitude
+  Serial.println(" ")
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
   mpu6050.update();
 
   if(millis() - timer > 1000){
     
-    
-    Serial.print("angleX : ");            // print 
+    // print Gyroscope X and Y angle
+    Serial.print("angleX : ");            
     Serial.print(mpu6050.getAngleX());
     Serial.print("  angleY : ");
     Serial.println(mpu6050.getAngleY());
@@ -51,7 +91,7 @@ void loop() {
       
        //x-axis / roll
       
-  if( mpu6050.getAngleX() == -1<0< 1){                  
+  if( mpu6050.getAngleX() == 0){                  
    servo2.write(90);    //set to 90 degrees
     }
    servo2.write(90 - mpu6050.getAngleY()); //unstable angle
